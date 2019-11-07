f787963491459c52bdc35c7e0fafc37db895c31c
Rajiv Singh <rajivpaulsingh@gmail.com>
2019-11-07 13:35:13 -0800

    A simple test with 4 steps

--- src/Basics.java
+++ src/Basics.java
@@ -2,12 +2,24 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 public class Basics extends Base {
 
     public static void main(String args[]) throws MalformedURLException {
 
-       AndroidDriver<AndroidElement> driver = Capabilities();
+        AndroidDriver<AndroidElement> driver = Capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //xpath, id, className, androidUIAutomator
+        /*
+         *  //tagName[@attribute='value']
+         */
+
+        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+        driver.findElementById("android:id/checkbox").click();
+        driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
 
     }
 }
