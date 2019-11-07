91abe4450f9e624d4108d602e98ae3c1204bac73
Rajiv Singh <rajivpaulsingh@gmail.com>
2019-11-07 11:46:48 -0800

    Converted the base class to function

--- src/Base.java
+++ src/Base.java
@@ -1,6 +1,6 @@
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
@@ -9,7 +9,7 @@ import java.net.URL;
 
 public class Base {
 
-    public static void main(String args[]) throws MalformedURLException {
+    public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
 
         File file = new File("src");
         File file1 = new File(file, "ApiDemos-debug.apk");
@@ -18,8 +18,9 @@ public class Base {
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Rajivemulator");
         desiredCapabilities.setCapability(MobileCapabilityType.APP, file1.getAbsolutePath());
 
-        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        AndroidDriver<AndroidElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
 
+        return driver;
 
     }
 }
