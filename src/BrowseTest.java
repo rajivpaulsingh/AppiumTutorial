5cb43c6c68fbcc72cdc57c6a66b6e6b94ae6b948
Rajiv Singh <rajivpaulsingh@gmail.com>
2019-11-15 14:19:33 -0800

    Updates to the test for mobile browser

--- src/BrowseTest.java
+++ src/BrowseTest.java
@@ -12,6 +12,8 @@ public class BrowseTest extends BaseChrome {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get("http://google.com");
+        driver.findElementByName("q").sendKeys("selenium");
+        driver.findElementByXPath("//*[@id=\'tsf\']/div[2]/div[1]/div[1]/button[2]/div/span/svg").click();
 
     }
 }
