fb99f40b28b63680c497be38a1e94cc793f2c667
Rajiv Singh <rajivpaulsingh@gmail.com>
2019-11-19 14:18:07 -0800

    Updated the test with mobile gestures

--- src/Ecommerce_TC_04.java
+++ src/Ecommerce_TC_04.java
@@ -1,11 +1,21 @@
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.testng.Assert;
 
 import java.net.MalformedURLException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+
+//This needs to be manually imported, it does not comes in by default
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 
 public class Ecommerce_TC_04 extends Base2 {
 
@@ -27,7 +37,7 @@ public class Ecommerce_TC_04 extends Base2 {
         driver.findElements(By.xpath("//*[@text='ADD TO CART']")).get(0).click();
 
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
-        Thread.sleep(5000);
+        Thread.sleep(3000);
 
         String amount1 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(0).getText();
         double price1 = getAmount(amount1);
@@ -43,6 +53,20 @@ public class Ecommerce_TC_04 extends Base2 {
         System.out.println("Total value of the products is: " + actualTotalPrice);
 
         Assert.assertEquals(expectedTotalPrice, actualTotalPrice);
+
+        //Mobile gestures - long press
+        WebElement checkbox = driver.findElement(By.className("android.widget.CheckBox"));
+        WebElement termsAndConditions = driver.findElement(By.xpath("//*[@text='Please read our terms of conditions']"));
+
+        TouchAction t = new TouchAction(driver);
+        t.tap(tapOptions().withElement(element(checkbox))).perform();
+        t.longPress(longPressOptions().withElement(element(termsAndConditions)).withDuration(Duration.ofSeconds(2))).release().perform();
+
+        //Close the popup
+        driver.findElement(By.id("android:id/button1")).click();
+
+        //Goto webview
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
 
     }
 
