446202f4179f33f00c5dce2398a39e7cc6acee1a
Rajiv Singh <rajivpaulsingh@gmail.com>
2019-11-19 14:44:16 -0800

    Created test to automate the native app and web

--- src/Ecommerce_TC_05.java
+++ src/Ecommerce_TC_05.java
@@ -1,11 +1,11 @@
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.TouchAction;
-
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.touch.TouchActions;
 import org.testng.Assert;
 
 import java.net.MalformedURLException;
@@ -13,12 +13,13 @@ import java.time.Duration;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-//This needs to be manually imported, it does not comes in by default
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 import static io.appium.java_client.touch.TapOptions.tapOptions;
 import static io.appium.java_client.touch.offset.ElementOption.element;
-import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
 
-public class Ecommerce_TC_04 extends Base2 {
+//This needs to be manually imported, it does not comes in by default
+
+public class Ecommerce_TC_05 extends Base2 {
 
     public static void main (String args[]) throws MalformedURLException, InterruptedException {
 
@@ -40,42 +41,27 @@ public class Ecommerce_TC_04 extends Base2 {
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
         Thread.sleep(3000);
 
-        String amount1 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(0).getText();
-        double price1 = getAmount(amount1);
-
-        String amount2 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(1).getText();
-        double price2 = getAmount(amount2);
-
-        double expectedTotalPrice = price1 + price2;
-        System.out.println("Sum of the products is: " + expectedTotalPrice);
-
-        String amount3 = driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
-        double actualTotalPrice = getAmount(amount3);
-        System.out.println("Total value of the products is: " + actualTotalPrice);
-
-        Assert.assertEquals(expectedTotalPrice, actualTotalPrice);
-
         //Mobile gestures - long press
         WebElement checkbox = driver.findElement(By.className("android.widget.CheckBox"));
-        WebElement termsAndConditions = driver.findElement(By.xpath("//*[@text='Please read our terms of conditions']"));
-
         TouchAction t = new TouchAction(driver);
         t.tap(tapOptions().withElement(element(checkbox))).perform();
-        t.longPress(longPressOptions().withElement(element(termsAndConditions)).withDuration(Duration.ofSeconds(2))).release().perform();
-
-        //Close the popup
-        driver.findElement(By.id("android:id/button1")).click();
 
         //Goto webview
         driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+        Thread.sleep(10000);
+
+        Set<String> contexts = driver.getContextHandles();
+        for(String contextName : contexts) {
+            System.out.println(contextName);
+        }
+
+        //Switch the context to web view
+        driver.context("WEBVIEW_com.androidsample.generalstore");
+        driver.findElement(By.name("q")).sendKeys("hello");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        driver.pressKey(new KeyEvent((AndroidKey.BACK)));
+
 
     }
 
-    public static double getAmount(String value) {
-
-        value = value.substring(1);
-        double amount2value = Double.parseDouble(value);
-        return amount2value;
-
-    }
 }
